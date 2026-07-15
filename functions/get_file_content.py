bb0633b5aa83386ca76d9782f51d103892c90693
a6st1n <juanagustin.sanchez26@gmail.com>
2026-07-15 12:15:00 -0300

    file content read

---
+++ functions/get_file_content.py
@@ -0,0 +1,28 @@
+import os
+
+
+def get_file_content(working_directory:str, file_path:str) -> str:
+    abs_path = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_path, file_path))
+    valid_path= os.path.commonpath([target_dir, abs_path]) == abs_path
+
+    if not valid_path:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(target_dir):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    max_characteres= 10000
+    try:
+
+        with open(target_dir, "r", encoding="utf-8") as f:
+            content= f.read(max_characteres)
+            if f.read(1) != "":
+                content += f'file "{file_path}" truncated to {max_characteres} characters'
+            return content
+        
+    except Exception as e:
+        
+        return f"Error: {e}"
+
+    
---
+++ test_get_file_content.py
@@ -0,0 +1,13 @@
+from functions.get_file_content import get_file_content
+
+#test case 1: valid file path
+result = get_file_content("calculator", "lorem.txt")
+print(f"lorem.txt length: {len(result)}")
+print(f"lorem.txt truncated: {'truncated' in result}")
+
+
+#rest of cases
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
